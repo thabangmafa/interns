695510c207bb5ff7e913e2a8095f90fd9cd40c8c
Thabang Mafa <thabang.mafa@gmail.com>
2021-07-23 23:29:55 +0200

    Move header information into a seperate file

---
+++ admin/connect.php
@@ -0,0 +1,18 @@
+<?php
+function OpenCon()
+{
+	$dbhost = "redcaplive.hsrc.ac.za";
+	$dbuser = "tmafa";
+	$dbpass = "@Thabang12";
+	$db = "interns_portal";
+	$conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or
+	die("Connect failed: %s\n". $conn->error);
+
+	return $conn;
+}
+
+function CloseCon($conn)
+{
+	$conn->close();
+}
+?>
--- index.php
+++ index.php
@@ -1,43 +1,12 @@
 <?php 
 
-
 include 'admin/connect.php';
-$conn = OpenCon();
-
-if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
-	
-	}else{
-     header("Location: login.php");
-     exit();
-}
-
 
 $menu_item = "1";
 $title = "";
 
  ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HSRC Interns Management Module</title>
-
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/bootstrap.css">
-
-    <link rel="stylesheet" href="assets/vendors/iconly/bold.css">
-
-    <link rel="stylesheet" href="assets/vendors/perfect-scrollbar/perfect-scrollbar.css">
-    <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
-    <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
-</head>
-
-<body>
-    <div id="app">
+	<?php require_once("admin/header.php"); ?>
 	<?php require_once("menu.php"); ?>
 
         <div id="main">
