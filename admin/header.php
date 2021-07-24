23309709d1f8d7e6cf2deb596d66f438e575b7ab
Thabang Mafa <thabang.mafa@gmail.com>
2021-07-24 11:12:43 +0200

    Add favicon to the portal

--- admin/header.php
+++ admin/header.php
@@ -38,7 +38,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
     <link rel="stylesheet" href="assets/vendors/bootstrap-icons/bootstrap-icons.css">
 
     <link rel="stylesheet" href="assets/css/app.css">
-    <link rel="shortcut icon" href="assets/images/favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo/favicon.ico" type="image/x-icon">
 </head>
 
 <body>
