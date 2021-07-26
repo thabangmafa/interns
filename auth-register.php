30d2b3d22a9f12cd0e36da2eb65f6bfbbb5d0e3c
Thabang Mafa <thabang.mafa@gmail.com>
2021-07-26 13:05:09 +0200

    Add user type to registration page

--- auth-register.php
+++ auth-register.php
@@ -35,12 +35,14 @@ if (isset($_POST['Username']) && isset($_POST['Password'])
 	else if(empty($email)){
 		$error = 'Email is required.';
 	}
+	else if(empty($user_type)){
+		$error = 'User type is required.';
+	}
 	else if($pass !== $re_pass){
 		$error = 'The confirmation password  does not match.';
 	}
 
-	else{
-
+	else{ 
 		// hashing the password
         $pass = md5($pass);
 
@@ -65,6 +67,7 @@ if (isset($_POST['Username']) && isset($_POST['Password'])
 			$error = $error;
 		}else {
            $sql2 = "INSERT INTO users(username, password, email, user_type) VALUES('$uname', '$pass', '$email', '$user_type')";
+
            $result2 = mysqli_query($conn, $sql2);
            if ($result2) {
 			   
@@ -123,25 +126,13 @@ if (isset($_POST['Username']) && isset($_POST['Password'])
                                 <i class="bi bi-envelope"></i>
                             </div>
                         </div>
-						<div class="col-md-6 col-12">
-							<div class="form-group">
-								<label for="last-name-column">User Type</label>
-								<fieldset class="form-group">
-							<select class="form-select" id="user_type" name="user_type">
-								<option value="4">Intern</option>
-								<option value="3">Mentor</option>
-								<option value="2">Host Institution Admin</option>
-								<option value="1">System Admin</option>
-							</select>
-						</fieldset>
-							</div>
-						</div>
-                        <div class="form-group position-relative has-icon-left mb-4">
+						<div class="form-group position-relative has-icon-left mb-4">
                             <input type="text" class="form-control form-control-xl" placeholder="Username" name="Username" id="Username" required="required">
                             <div class="form-control-icon">
-                                <i class="bi bi-person"></i>
+                                <i class="bi bi-envelope"></i>
                             </div>
                         </div>
+                        
                         <div class="form-group position-relative has-icon-left mb-4">
                             <input type="password" class="form-control form-control-xl" placeholder="Password" name="Password" id="Password" required="required">
                             <div class="form-control-icon">
@@ -153,6 +144,16 @@ if (isset($_POST['Username']) && isset($_POST['Password'])
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
+                        </div>
+						<div class="form-group position-relative has-icon-left mb-4">
+                            <select class="form-select" id="user_type" name="user_type" required="required">
+								<option value="">Select user type</option>
+								<option value="4">Intern</option>
+								<option value="3">Mentor</option>
+								<option value="2">Host Institution Admin</option>
+								<option value="1">System Admin</option>
+							</select>
+                            
                         </div>
                         <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Register</button>
                     </form>
