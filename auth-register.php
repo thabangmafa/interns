9d9a2ec55c43e313dbd0ab1bcaf3cbb2cbb54f01
Thabang Mafa <thabang.mafa@gmail.com>
2021-07-26 13:48:41 +0200

    Add user type to registration page

--- auth-register.php
+++ auth-register.php
@@ -129,7 +129,7 @@ if (isset($_POST['Username']) && isset($_POST['Password'])
 						<div class="form-group position-relative has-icon-left mb-4">
                             <input type="text" class="form-control form-control-xl" placeholder="Username" name="Username" id="Username" required="required">
                             <div class="form-control-icon">
-                                <i class="bi bi-envelope"></i>
+                                <i class="bi bi-grid-fill"></i>
                             </div>
                         </div>
                         
@@ -146,14 +146,16 @@ if (isset($_POST['Username']) && isset($_POST['Password'])
                             </div>
                         </div>
 						<div class="form-group position-relative has-icon-left mb-4">
-                            <select class="form-select" id="user_type" name="user_type" required="required">
+                            <select class="form-control form-control-xl form-select" id="user_type" name="user_type" required="required">
 								<option value="">Select user type</option>
 								<option value="4">Intern</option>
 								<option value="3">Mentor</option>
 								<option value="2">Host Institution Admin</option>
 								<option value="1">System Admin</option>
 							</select>
-                            
+                            <div class="form-control-icon">
+                                <i class="bi bi-stack"></i>
+                            </div>
                         </div>
                         <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Register</button>
                     </form>
