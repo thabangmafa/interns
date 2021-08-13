10e28c9a1cff4e12031088d08b3af71bdc5e92f5
Thabang Mafa <thabang.mafa@gmail.com>
2021-08-13 12:13:37 +0200

    generate date of birth from ID number

--- user-registration.php
+++ user-registration.php
@@ -22,10 +22,20 @@ if (isset($_POST['Submit'])) {
 	}
 	$id = $_SESSION['id'];
 	
-	//$date_of_birth = validate($_POST['date_of_birth']);
+	
+	$id_number = validate($_POST['id_number']);
+	
+	$oldstr = substr($id_number,0,6);
+
+	$newstr = substr_replace($oldstr, '-', 2, 0);
+	$newstr = substr_replace($newstr, '-', 5, 0);
+	$time = strtotime($newstr);
+	$dob = date('Y-m-d',$time);
+	
+	$date_of_birth = $dob;
 	$gender = validate($_POST['gender']);
 	$race = validate($_POST['race']);
-	$id_number = validate($_POST['id_number']);
+	
 	$id_type = validate($_POST['id_type']);
 	$citizenship = validate($_POST['citizenship']);
 	$country = validate($_POST['country']);
@@ -41,6 +51,9 @@ if (isset($_POST['Submit'])) {
 	$insert1 = '';
 	$insert2 = '';
 	$update = '';
+	
+	
+	
 	
 	if(isset($_FILES['id_document']['name'])){
 
@@ -81,6 +94,7 @@ if (isset($_POST['Submit'])) {
 	
 	$sql2 = "INSERT INTO RegistrationDetails(
 						UserID,
+						DateOfBirth,
 						Gender,
 						Race,
 						IDNumber,
@@ -99,7 +113,7 @@ if (isset($_POST['Submit'])) {
 						$insert1
 ) VALUES(
 						'$id',
-						
+						'$date_of_birth',
 						'$gender',
 						'$race',
 						'$id_number',
@@ -131,7 +145,7 @@ if (isset($_POST['Submit'])) {
 	}else{
 		
 	$sql2 = "UPDATE RegistrationDetails SET 
-						
+						DateOfBirth = '$date_of_birth',
 						Gender = '$gender',
 						Race = '$race',
 						IDNumber = '$id_number',
@@ -168,7 +182,7 @@ if (isset($_POST['Submit'])) {
 
 	while($userdetails = mysqli_fetch_array($result)) {
 
-			
+
 			$UserGender = $userdetails['Gender'];
 			$UserRace = $userdetails['Race'];
 			$UserIDNumber = $userdetails['IDNumber'];
@@ -421,15 +435,7 @@ if (isset($_POST['Submit'])) {
                                                 </div>
 												
 												
-												
-												
-                                                <!--div class="col-md-6 col-12">
-                                                    <div class="form-group">
-                                                        <label for="date_of_birth">Date of Birth <span style="color:red">*</span></label>
-                                                        <input type="date" id="date_of_birth" class="form-control"
-                                                            name="date_of_birth" value="<?php //echo @$UserDateOfBirth; ?>" required="required">
-                                                    </div>
-                                                </div-->
+								
 												
 												<div class="col-md-6 col-12">
                                                     <div class="form-group">
